922f43687458265862ca5aa07ada130215eea7bf
housiadas <housiadas@gmail.com>
2016-09-30 01:24:33 +0300

    nothing is working

--- index.android.js
+++ index.android.js
@@ -9,13 +9,15 @@ import {
   TouchableOpacity
 } from 'react-native';
 
-import MapView from 'react-native-maps';
+//import MapView from 'react-native-maps';
+
+var myNumber, othersNumber;
 
 class PicMe extends Component {
 
-    clickMe() {
+    /*clickMe() {
         navigator.geolocation.getCurrentPosition(function(position) {
-          fetch('http://192.168.1.9:3000/new', {
+          fetch('http://192.168.1.9:3000/newGame', {
             method: 'POST',
             headers: {
               'Content-Type': 'application/json',
@@ -31,14 +33,63 @@ class PicMe extends Component {
           })
           .catch((error) => { console.error(error); });
         });
+    }*/
+
+    testMe() {
+        fetch('http://picmetest.herokuapp.com/newGame', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                //playerId: '57ed6f27dcba0f1f2b138b98' // eugene
+                playerId: '57ed6f83dcba0f1f2b138bb9' // yanis
+            })
+        })
+            .then((response) => response.json())
+            .then((responseJson) => {
+                alert(responseJson.wait);
+                alert(responseJson.yourRandom);
+                if (responseJson.wait && responseJson.yourRandom) {
+                    myNumber = responseJson.yourRandom;
+                    othersNumber = responseJson.othersRandom;
+                    // sleep for 2 seconds
+                    setTimeout(
+                        () => {
+                            console.log('I do not leak!');
+                            alert("onene");
+                            this.testMe();
+                        },
+                        5000
+                    );
+                } else if (responseJson.wait) {
+                    // sleep for 2 seconds
+                    setTimeout(
+                        () => {
+                            console.log('I do not leak!');
+                            alert("sakis");
+                            this.testMe();
+                        },
+                        500
+                    );
+                } else if (!responseJson.wait) {
+                    alert(myNumber + " " + othersNumber);
+                } else
+                    alert("Error");
+            })
+            .catch((error) => {
+                console.error(error);
+            });
     }
 
     render() {
+        let pic = {
+            uri: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg'
+        };
         return (
-                <MapView
-                    style = { styles.map }
-                    showsUserLocation = {true}
-                />
+            <TouchableOpacity onPress ={this.testMe()}>
+                <Image source={pic} style={{width: 193, height: 110}}/>
+            </TouchableOpacity>
         );
     }
 
